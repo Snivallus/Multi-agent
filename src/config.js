c6082e0e7379106ec8b9f56ff13e8be3c6a3a0c9
Snivellus Snape <21307140051@m.fudan.edu.cn>
2025-03-23 10:26:07 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
     // 这是由 ngrok 生成的隧道 URL (穿透本地5000端口)
-    apiBaseUrl: 'https://476a-2001-da8-8001-825-2413-8f6e-59e5-5280.ngrok-free.app'
+    apiBaseUrl: 'https://f299-2001-da8-8001-825-20e6-ec7b-6356-238.ngrok-free.app'
   };
   
   export default config;
