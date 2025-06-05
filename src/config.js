31f6eede79888e9042141dfb338c47a718f4c2dd
Snivellus Snape <21307140051@m.fudan.edu.cn>
2025-06-05 15:26:40 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,10 @@
 const config = {
+    // 用于调用与 DataBase 相关的 API 
+    apiBaseUrl_1: 'http://localhost:8000',
+    // 用于调用与 Agent 相关的 API
     // 这是由 ngrok 生成的隧道 URL (穿透本地5000端口: http://127.0.0.1:5000)
     // apiBaseUrl: 'https://d64e-240e-46c-5210-889-d849-c3da-a246-e88b.ngrok-free.app'
-    apiBaseUrl_1: 'http://localhost:8000',
-    apiBaseUrl_2: 'http://localhost:5000'
+    apiBaseUrl_2: 'http://localhost:5000'  
   };
   
   export default config;
